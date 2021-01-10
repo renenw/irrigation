5bf3f34cb0820793408f7a4d877f1ade0b7da8d3
renen <renen@121.co.za>
2021-01-10 16:58:48 +0200

    initial working code

---
+++ initialiser.rb
@@ -0,0 +1,99 @@
+require 'json'
+require 'syslog/logger'
+require 'date'
+
+@log = Syslog::Logger.new 'IRRIGATION_INITIALISER'
+
+INSTRUCTIONS_DIRECTORY = '/home/pi/irrigation_instructions'
+
+CIRCUITS               = %w(pond_ferns front_misters front front_fynbos outhouse_lawn driveway vegetable_patch jungle_gym pool_beds pool_lawn pool front_lawn trees)
+
+# Based on day, write an instruction to the IRRIGATION_INSTRUCTIONS directory.
+# Instruction indicates that, if conditions allow, irrigation should be triggered.
+# Instruction parameters include:
+#   duration_seconds: run the irrigation for this number of seconds
+#   circuit_name: the circuit to be run
+#   ignore: %w(wind wet temperature time_of_day): override rules so that circuit triggers irrespective of, for example, wether the wind is blowing
+
+def main
+  CIRCUITS.map(&:to_sym).each do |circuit|
+    instruction = self.send(circuit)                                                 if respond_to?(circuit, true)
+    write({ circuit_name: circuit, created_at: Time.new.ctime }.merge(instruction))  if instruction
+    log("#{circuit} instruction created.")                                           if instruction
+  end
+end
+
+
+def pond_ferns
+  {
+#    duration_seconds: 180,
+    duration_seconds: 60,
+    description:      'Next to pond, under yellow wood (daily) [6]',
+    ignore:           'all',
+  }
+end
+
+def front_misters
+  {
+#    duration_seconds: 180,
+    duration_seconds: 60,
+    description:      'Next to driveway, under jacaranda (daily) [2]',
+    ignore:           %w(hot time_of_day windy wet),
+  }
+end
+
+def front
+  {
+#    duration_seconds: 1200,
+    duration_seconds: 60,
+    description:      'Front Beds, under jacaranda (every third day) [1]',
+    ignore:           %w(windy time_of_day),
+  }  if is_day_n?(2)
+end
+
+def front_fynbos
+  {
+#    duration_seconds: 1200,
+    duration_seconds: 60,
+    description:      'Front beds round pond and in front of house (every third day) [0]',
+  }  if is_day_n?(2)
+end
+
+def pool_beds
+  {
+#    duration_seconds: 1200,
+    duration_seconds: 60,
+    description:      'Beds down past pool (every third day) [5]',
+    ignore:           %w(hot),
+  }  if is_day_n?(2)
+end
+
+
+
+def write(instruction)
+  File.write("#{INSTRUCTIONS_DIRECTORY}/#{instruction[:circuit_name]}.json", instruction.to_json)
+end
+
+def log(line)
+  @log.info(line)
+end
+
+def is_day_of_week?(days)
+  days.include?(Time.new.strftime('%A').downcase)
+end
+
+# if n==3, returns true every third day.
+def is_day_n?(n)
+  (Time.new.yday % n)==0
+end
+
+def test
+  pp 'day of week'
+  pp is_day_of_week?(%w(monday))
+  pp 'every 3rd day'
+  pp is_day_n?(3)
+  pp 'every 2nd day'
+  pp is_day_n?(2)
+end
+
+main
---
+++ runner.rb
@@ -0,0 +1,149 @@
+require 'json'
+require 'syslog/logger'
+require "uri"
+require "net/http"
+# require 'date'
+
+@log = Syslog::Logger.new 'IRRIGATION_RUNNER'
+
+INSTRUCTIONS_DIRECTORY            = '/home/pi/irrigation_instructions'
+SWITCHER_URL                      = 'http://192.168.0.203/'
+RAINY_DAY_PRECIPITATION_THRESHOLD = 2.5
+CONFIG                            = JSON.parse(File.read('/home/pi/irrigation/config.json'))
+
+# Based instructions in the IRRIGATION_INSTRUCTIONS directory....
+# Trigger irrigations if conditions allow (not conditions may be ignored using the ignore parameter):
+#   windy:        wait to see if the weather improves
+#   hot:          wait for it to cool down
+#   wet:          bin the instruction
+#   time_of_day:  by default, don't run during the middle of the day
+
+def process_instructions
+  instructions = Dir.children(INSTRUCTIONS_DIRECTORY)
+  log("#{instructions.count} instructions found.")
+  rule_params  = initialise_rules  if !instructions.empty?
+  Dir.children(INSTRUCTIONS_DIRECTORY).each do |file|
+    file_name   = "#{INSTRUCTIONS_DIRECTORY}/#{file}"
+    instruction = JSON.parse(File.read(file_name), symbolize_names: true)
+    process_instruction(file_name, instruction, rule_params)
+  end
+end
+
+def process_instruction(file_name, instruction, rule_params)
+  if allowed_to_run?(instruction, rule_params)
+    if rule_params[:running]
+      log("Currently irrigating. Skipping #{instruction[:circuit_name]} for now.")
+    else
+      rule_params[:running] = true
+      trigger_circuit(instruction)
+      File.delete(file_name)
+    end
+  else
+    # if this instruction has been blocked because it is wet, delete the instruction
+    if rule_params[:wet] && (instruction[:ignore] || []).include?('wet')
+      log("Its wet, so not going to run #{instruction[:circuit_name]}.")
+      File.delete(file_name)
+    end
+  end
+end
+
+def trigger_circuit(instruction)
+
+  circuit  = instruction[:circuit_name]
+  duration = instruction[:duration_seconds]
+  switch   = CONFIG[circuit]
+
+  if switch
+    url      = URI("#{SWITCHER_URL}on?switch=#{switch}&duration=#{duration}")
+    http     = Net::HTTP.new(url.host, url.port);
+    request  = Net::HTTP::Get.new(url)
+    response = http.request(request)
+    response.read_body
+    log("#{circuit} (switch #{switch}) switched on for #{duration} seconds.")
+  else
+    log("Switch number not found: #{circuit}")
+  end
+
+end
+
+def delete_instruction(file_name)
+  File.delete(file_name)
+end
+
+def allowed_to_run?(instruction, rule_params)
+  run    = !rule_params[:running]
+  if run
+    ignore = instruction[:ignore] || []
+    if ignore!='all'
+      assess = (rule_params.keys - ignore.map(&:to_sym) )
+      if !assess.empty?
+        run = !rule_params.fetch_values( *assess ).any?
+        if !run
+          log(instruction[:circuit_name] + ': ' + rule_params.select { |k,v| v && assess.include?(k) }.keys.join(', ') + ' block from running.')
+        end
+      end
+    end
+  end
+  run
+end
+
+def main
+  if are_any_circuits_running?
+    log('Irrigation currently running. Not going to process any instructions.')
+  else
+    process_instructions
+  end
+end
+
+def initialise_rules
+  {
+    running:     false,
+    time_of_day: (8..14).include?(Time.now.utc.hour),
+  }.merge(weather_rules)
+end
+
+def weather_rules
+
+  weather = load_current_weather
+
+  windy   = (weather[:hourly][:wind_speed][0]   >  5)
+  hot     = (weather[:hourly][:temperatures][0] > 30)
+
+  wet     = false
+  wet     = true  if weather[:today][:precipitation]      > RAINY_DAY_PRECIPITATION_THRESHOLD
+  wet     = true  if weather[:three_days][:precipitation] > RAINY_DAY_PRECIPITATION_THRESHOLD * 2
+  wet     = true  if weather[:week][:precipitation]       > RAINY_DAY_PRECIPITATION_THRESHOLD * 4
+
+  {
+    windy:  windy,
+    hot:    hot,
+    wet:    wet,
+  }
+
+end
+
+
+def load_current_weather
+  source  = `ruby /home/pi/yr_parser/yr.rb --latitude -33.95283 --longitude 18.48056 --msl 11`
+  weather = JSON.parse(source, symbolize_names: true)
+end
+
+
+def are_any_circuits_running?
+  !(request_switcher_status=~/^Switch:\s\d+:\sOn/).nil?
+end
+
+def request_switcher_status
+  url      = URI(SWITCHER_URL)
+  http     = Net::HTTP.new(url.host, url.port);
+  request  = Net::HTTP::Get.new(url)
+  response = http.request(request)
+  response.read_body
+end
+
+
+def log(line)
+  @log.info(line)
+end
+
+main
